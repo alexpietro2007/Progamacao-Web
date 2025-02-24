eb43fe57b74d864752b6e0d1d56d2e9feb6ff280
Alex Pietro <alexpieetro@gmail.com>
2025-02-24 12:15:56 -0300

    Aoooooooo

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -1,1 +1,19 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import session, { Cookie } from 'express-session';
+
+const cl = console.log
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const app = express()
+
+app.use('./public', express.static(path.join(__dirname, 'public')))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+}
+)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App rodando na porta localhost:${PORT}`));
